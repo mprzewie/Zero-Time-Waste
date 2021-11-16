2e6a316be978b83a64e9337a3526e4bdf60f9c91
Marcin Przewięźlikowski <m.przewie@gmail.com>
2021-11-17 00:19:40 +0100

    tryout experiments with differenct architectures

---
+++ ztw/experiments/single_arch_transfer.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -xe
+
+declare -A num_heads
+
+num_heads=( ["resnet56"]=27 ["vgg16bn"]=14 ["wideresnet32_4"]=15 ["mobilenet"]=13 ["tv_resnet"]=5 )
+
+if [[ -n $3 ]]; then
+  seed=$3
+else
+  seed=1666
+fi
+echo "seed: $seed"
+
+if [ -z "$1" ] | [ -z "$2" ]; then
+  echo "No arch or dataset passed!"
+  exit 1
+fi
+
+arch=$1
+dataset=$2
+heads="${num_heads[$arch]}"
+echo "arch: $arch dataset: $dataset heads: $heads"
+
+base_net="${seed}_${dataset}_${arch}_base_cnn"
+cmd_base="python train_networks.py -a $arch -s $seed --heads all --skip_train_logits --save_test_logits"
+
+$cmd_base -t cnn --tag $base_net -d $dataset
+
+for transfer_dataset in cifar10 cifar100 ; do
+  $cmd_base -t sdn_ic --head_arch conv sdn_pool -d $transfer_dataset \
+    --override_cnn_to_tune $base_net \
+    --tag "${seed}_${dataset}_${arch}_sdn_${dataset}_transfer_to_${transfer_dataset}"
+done
---
+++ ztw/experiments/transfer_experiments.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#SBATCH --job-name=std_devs
+#SBATCH --qos=normal
+#SBATCH --gres=gpu:1
+#SBATCH --mem=60G
+#SBATCH --cpus-per-task 10
+
+##SBATCH --job-name=std_devs
+##SBATCH --qos=big
+##SBATCH --gres=gpu:2
+##SBATCH --mem=120G
+##SBATCH --cpus-per-task 20
+
+seed_displcmt=200
+num_seeds=1
+max_iter=$(($num_seeds / 2))
+
+for arch in resnet56 vgg16bn wideresnet32_4 mobilenet; do
+  for dataset in cifar10 cifar100 ; do
+    for i in $(seq 0 $max_iter); do
+      seed1=$(($i + $seed_displcmt))
+      seed2=$(($max_iter + $i + $seed_displcmt))
+      CUDA_VISIBLE_DEVICES=0 bash ./experiments/single_arch_transfer.sh $arch $dataset $seed1 #&
+      #CUDA_VISIBLE_DEVICES=1 bash ./experiments/single_arch.sh $arch $dataset $seed2 &
+      wait
+    done
+  done
+done
