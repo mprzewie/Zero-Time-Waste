9f5d4560246d3efbfcb023554b04c73da59f6afd
Marcin Przewięźlikowski <m.przewie@gmail.com>
2021-11-25 14:16:23 +0100

    tupto experiments

--- ztw/experiments/transfer_up_to.sh
+++ ztw/experiments/transfer_up_to.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+set -xe
 
 CMD="python train_networks.py -a vgg16bn -s 200 --heads all --skip_train_logits --save_test_logits -t cnn --relearn_final_layer
 "
@@ -11,7 +11,7 @@ do
     for UPTO in 2 4 6 8 10 12;
     do
       SUF=${FROM_D}_to_${TO_D}_upto_${UPTO}
-      $CMD -d TO_D --override_cnn_to_tune ${FROM_D}_vgg16bn_cnn --freeze_cnn_up_to $UPTO --suffix $SUF --tag $SUF
+      $CMD -d $TO_D --override_cnn_to_tune ${FROM_D}_vgg16bn_cnn --freeze_cnn_up_to $UPTO --suffix $SUF --tag $SUF
     done
   done
 done
