53575d643dff44ae6f72ef7daf43754ee81859fa
Marcin Przewięźlikowski <m.przewie@gmail.com>
2021-11-28 12:30:05 +0100

    fix to state dict loading

--- ztw/network_architectures.py
+++ ztw/network_architectures.py
@@ -6,9 +6,11 @@
 import os
 import os.path
 import pickle
+from typing import Dict, OrderedDict
 
 import neptune
 import torch
+from torch import nn
 
 from architectures.CNNs.MobileNet import MobileNet
 from architectures.CNNs.ResNet import ResNet
@@ -84,7 +86,7 @@ def save_networks(args, model_name, model_params, models_path, save_type):
             print(f"I will override the following parameters of {cnn_name} with params from {args.override_cnn_to_tune}")
             print(m2_state_dict.keys())
 
-            model.load_state_dict(m2_state_dict, strict=False)
+            load_state_dict_with_mismatches(model, m2_state_dict)
 
 
         save_model(args, model, model_params, models_path, cnn_name, epoch=0)
@@ -490,20 +492,23 @@ def load_model(args, models_path, model_name, epoch=0):
         load_path = network_path + '/' + str(epoch)
 
     load_state_dict = torch.load(load_path)
+    model = load_state_dict_with_mismatches(model, load_state_dict)
+
+    return model, model_params
+
+def load_state_dict_with_mismatches(model: nn.Module, state_dict: OrderedDict[str, torch.Tensor]) -> nn.Module:
     model_state_dict = model.state_dict()
     keys_to_del = []
-    for k, v in load_state_dict.items():
+    for k, v in state_dict.items():
         if k in model_state_dict:
             if model_state_dict[k].shape != v.shape:
                 print("Size mismatch for", k, model_state_dict[k].shape, "vs", v.shape, "this param will be omitted")
                 keys_to_del.append(k)
-    for k in  keys_to_del:
-        del load_state_dict[k]
+    for k in keys_to_del:
+        del state_dict[k]
 
-    model.load_state_dict(load_state_dict, strict=False)
-
-    return model, model_params
-
+    model.load_state_dict(state_dict, strict=False)
+    return model
 
 def get_sdn(cnn):
     if (isinstance(cnn, VGG)):
