29223f4bbbe79261958ab45115cee6d1ca27f76c
Marcin Przewięźlikowski <m.przewie@gmail.com>
2021-12-09 12:44:08 +0100

    stacking

--- ztw/experiments/single_arch_transfer.sh
+++ ztw/experiments/single_arch_transfer.sh
@@ -28,8 +28,13 @@ cmd_base="python train_networks.py -a $arch -s $seed --heads all --skip_train_lo
 #$cmd_base -t cnn --tag "${seed}_${base_net}" -d $dataset
 
 for transfer_dataset in cifar10 cifar100 ; do
+#  $cmd_base -t sdn_ic --head_arch conv sdn_pool -d $transfer_dataset \
+#    --override_cnn_to_tune $base_net \
+#    --tag "${seed}_${dataset}_${arch}_sdn_${dataset}_transfer_to_${transfer_dataset}" \
+#    --suffix "transfer_from_${dataset}_new_ft_test"
+
   $cmd_base -t sdn_ic --head_arch conv sdn_pool -d $transfer_dataset \
-    --override_cnn_to_tune $base_net \
-    --tag "${seed}_${dataset}_${arch}_sdn_${dataset}_transfer_to_${transfer_dataset}" \
-    --suffix "transfer_from_${dataset}_new_ft_test"
+    --override_cnn_to_tune $base_net --stacking --detach_prev \
+    --tag "${seed}_${dataset}_${arch}_sdn-ic-stacking_${dataset}_transfer_to_${transfer_dataset}" \
+    --suffix "transfer_from_${dataset}_sdn-ic-stacking"
 done
