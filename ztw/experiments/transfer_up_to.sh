da8c9627eca281acda9e78caee9bb5ae4a4ca9b5
Marcin Przewięźlikowski <m.przewie@gmail.com>
2022-01-04 23:15:09 +0100

    upto experiments

--- ztw/experiments/transfer_up_to.sh
+++ ztw/experiments/transfer_up_to.sh
@@ -3,12 +3,12 @@ set -xe
 
 seed=200
 
-#arch="vgg16bn"
-arch="tv_resnet"
+#ARCH="vgg16bn"
+ARCH="tv_resnet"
 
-CMD="python train_networks.py -a $arch -s $seed --skip_train_logits --save_test_logits -t cnn"
+CMD="python train_networks.py -a $ARCH -s $seed --skip_train_logits --save_test_logits -t cnn"
 
-if [[ $arch == "tv_resnet" ]]; then
+if [[ $ARCH == "tv_resnet" ]]; then
   CMD="$CMD --heads quarter --head_arch conv_less_ch sdn_pool -p 4 --lr_scaler 2"
   ztw_arg="--detach_norm layernorm"
 else
@@ -20,22 +20,23 @@ fi
 
 for FROM_D in cifar10 cifar100;
 do
-  CNN_TO_TUNE=${FROM_D}_${arch}_cnn
+  CNN_TO_TUNE=${FROM_D}_${ARCH}_cnn
   for TO_D in oct2017 cifar10 cifar100 ;
   do
-    for UPTO in 2 4;
+    for UPTO in 1 2 4;
     do
+      SUF_BASE=${FROM_D}_to_${TO_D}_${ARCH}_upto_${UPTO}
 
-      SUF=${FROM_D}_to_${TO_D}_upto_${UPTO}_rest_copied
+      SUF="${SUF_BASE}_rest_copied"
       $CMD -d $TO_D --override_cnn_to_tune $CNN_TO_TUNE --freeze_cnn_up_to $UPTO --suffix $SUF --tag $SUF
 	
-      SUF=${FROM_D}_to_${TO_D}_upto_${UPTO}_rest_random
+      SUF="${SUF_BASE}_rest_random"
       $CMD -d $TO_D --override_cnn_to_tune $CNN_TO_TUNE --freeze_cnn_up_to $UPTO --override_cnn_up_to $UPTO --suffix $SUF --tag $SUF
 
-      SUF=${FROM_D}_to_${TO_D}_upto_${UPTO}_rest_copied-nofreeze
+      SUF="${SUF_BASE}_rest_copied-nofreeze"
       $CMD -d $TO_D --override_cnn_to_tune $CNN_TO_TUNE --suffix $SUF --tag $SUF
 
-      SUF=${FROM_D}_to_${TO_D}_upto_${UPTO}_rest_random-nofreeze
+      SUF="${SUF_BASE}_rest_random-nofreeze"
       $CMD -d $TO_D --override_cnn_to_tune $CNN_TO_TUNE --override_cnn_up_to $UPTO --suffix $SUF --tag $SUF
 
     done
